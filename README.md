0f5f8cef9754d7ab9e2cdae06b214e1852ee5839
Wing <wingyeung0317@gmail.com>
2023-01-01 15:32:23 +0800

    Green Card ui completed

--- README.md
+++
@@ -1,1 +0,0 @@
-# taxiMemo
---
+++ api.js
@@ -0,0 +1,6 @@
+// Search API:
+'https://maps.googleapis.com/maps/api/place/autocomplete/json?input='+user_input+'%20near%20hong%20kong&language=zh-HK&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc'
+
+// Result API:
+/*CHI*/'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,icon,formatted_address,geometry,place_id&input='+user_input+'&inputtype=textquery&language=zh-HK&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc'
+/*ENG*/'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,icon,formatted_address,geometry,place_id&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc'
