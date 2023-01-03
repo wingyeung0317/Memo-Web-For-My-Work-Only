cc1ee295178675288a86ca4dec17e57cf589327c
Wing <wingyeung0317@gmail.com>
2023-01-03 21:07:20 +0800

    netlify cors

--- load/index/insertEng.js
+++ load/index/insertEng.js
@@ -1,4 +1,4 @@
-fetch('findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc')
+fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc')
   .then((response) => response.json())
   .then(function (data) {
     $.each(data, function(i,item){
