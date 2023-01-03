8875d3f45ce59569d1136277e040f34edc2272ca
Wing <wingyeung0317@gmail.com>
2023-01-03 11:52:25 +0800

    Map Search Stability Update TODO: record UI, database

---
+++ load/index/insertEng.js
@@ -0,0 +1,9 @@
+$.getJSON('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc', function(data){
+  $.each(data, function(i,item){
+    $.each(item, function(i,place){
+      console.log(item);
+      $('.engLocName')[i].append(place.name);
+      $('.engStreetName')[i].append(place.formatted_address);
+    });
+  });
+});
