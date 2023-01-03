9c32423f1b7f096301d96f050618005554055a8d
Wing <wingyeung0317@gmail.com>
2023-01-03 20:10:18 +0800

    Test map cors

--- index.js
+++ index.js
@@ -117,7 +117,16 @@ function getDetail() {
   user_input = $('#pac-input').val();
   $('#detailInfo').html('');
   var resultNo = 0;
-  $.getJSON('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,icon,formatted_address&input='+user_input+'&inputtype=textquery&language=zh-HK&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc', function(data){
+  var axios = require('axios');
+
+  var config = {
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,icon,formatted_address&input='+user_input+'&inputtype=textquery&language=zh-HK&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc',
+    headers: { }
+  };
+
+  axios(config)
+  .then(function (data) {
     $.each(data, function(i,item){
       $.each(item, function(i,place){
         console.log(item);
@@ -129,6 +138,9 @@ function getDetail() {
         resultNo += 1;
       });
     });
+  })
+  .catch(function (error) {
+    console.log(error);
   });
   $.getScript("load/index/insertEng.js");
 }
--- load/index/insertEng.js
+++ load/index/insertEng.js
@@ -1,9 +1,21 @@
-$.getJSON('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc', function(data){
-  $.each(data, function(i,item){
-    $.each(item, function(i,place){
-      console.log(item);
-      $('.engLocName')[i].append(place.name);
-      $('.engStreetName')[i].append(place.formatted_address);
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc',
+  headers: { }
+};
+
+axios(config)
+.then(function (data) {
+    $.each(data, function(i,item){
+  $.each(item, function(i,place){
+    console.log(item);
+    $('.engLocName')[i].append(place.name);
+    $('.engStreetName')[i].append(place.formatted_address);
     });
   });
+})
+.catch(function (error) {
+  console.log(error);
 });
