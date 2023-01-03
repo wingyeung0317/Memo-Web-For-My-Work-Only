7068292a90304c71e09ea4fd17aad135559c889c
Wing <wingyeung0317@gmail.com>
2023-01-03 11:51:52 +0800

    Map Search Stability Update TODO: record UI, database

--- index.js
+++ index.js
@@ -93,17 +93,5 @@ function getDetail() {
       });
     });
   });
-  getEngDetail();
-}
-
-function getEngDetail() {
-  $.getJSON('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name,formatted_address&input='+user_input+'&inputtype=textquery&language=en&key=AIzaSyDUWWx5ghRSO8HOENqj1IRLGmv1HApkmfc', function(data){
-    $.each(data, function(i,item){
-      $.each(item, function(i,place){
-        console.log(item);
-        $('.engLocName')[i].append(place.name);
-        $('.engStreetName')[i].append(place.formatted_address);
-      });
-    });
-  });
+  $.getScript("load/index/insertEng.js");
 }
